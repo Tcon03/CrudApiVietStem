4f0aa2167b175764fc3c8db5177ca854d91e5ab1
VFAST-SERVER-SI <nguyentruong281203@gmail.com>
2025-08-25 02:52:55 -0700

    Test Api

--- CrudVietSteam/Model/ApiConfiguration.cs
+++ CrudVietSteam/Model/ApiConfiguration.cs
@@ -8,8 +8,8 @@ namespace CrudVietSteam.Model
 {
     public class ApiConfiguration
     {
-        public string BaseUrl { get; set; } = "http://localhost:3000/explorer/";
-        //public string BaseUrl { get; set; } = "http://192.168.10.47:3000";
+        //public string BaseUrl { get; set; } = "http://localhost:3000/explorer/";
+        public string BaseUrl { get; set; } = "http://192.168.10.47:3000";
         public string LoginEndpoint { get; set; } = "/api/Accounts/login";
         public string GetContestEndpoint { get; set; } = "/api/Contests";
         public string GetCityEndpoint { get; set; } = "/api/Cities";
